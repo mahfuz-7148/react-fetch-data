92e70b448ffc883c86a2b51240fe068f5054d1da
mahfuz-7148 <mrahman7148@gmail.com>
2024-12-24 02:34:22 +0600

    'up'

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import { useState } from 'react'
+
 import './App.css'
-import { useEffect } from 'react'
-import UserData from './pages/UserData'
+
 
 function App() {
 
-  const [allData, setAllData] = useState([])
+ 
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => setAllData(data))
-  },[])
 
   return (
 
-    <div className='border-2 border-blue-600 grid grid-cols-2 p-10 m-10'>
-     { allData.map(data => 
-       <UserData 
-       key={data.id}
-       data = {data}
-       />
-    )}
+    <div>
+
     </div>
-  )
+  ) 
 }
 
 export default App
